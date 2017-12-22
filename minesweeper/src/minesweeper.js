de2259752e5020d17477a863a1b2fbaa7672ba32
Ryan Leys <heyryanleys@gmail.com>
2017-12-22 14:14:13 -0500

    Starting minesweeper project

---
+++ minesweeper/src/minesweeper.js
@@ -0,0 +1,7 @@
+const blankLine = ' | | '
+const guessLine = '1| | '
+const bombLine = ' |B| '
+console.log('This is what a board with a guess and a bomb would look like');
+console.log(guessLine);
+console.log(blankLine);
+console.log(bombLine);
