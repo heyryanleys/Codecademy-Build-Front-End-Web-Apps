d42bc3534e210edf77324dab7b4e4acbe4e9ca43
heyryanleys <ryanleys@gmail.com>
2017-12-27 10:30:07 -0500

    Function now creates board

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,1 +1,14 @@
-const board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+const printBoard = board =>{
+  console.log('Current Board:');
+  console.log(board[0].join('|'));
+  console.log(board[1].join('|'));
+  console.log(board[2].join('|'));
+};
+
+let board = [
+  [' ',' ',' '],
+  [' ',' ',' '],
+  [' ',' ',' ']
+];
+
+printBoard(board);
