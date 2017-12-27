984755f431629b75c1d71a017e1cfcef84947e6a
heyryanleys <ryanleys@gmail.com>
2017-12-27 10:18:42 -0500

    Making board dynamic

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,7 +1,1 @@
-const blankLine = ' | | '
-const guessLine = '1| | '
-const bombLine = ' |B| '
-console.log('This is what a board with a guess and a bomb would look like');
-console.log(guessLine);
-console.log(blankLine);
-console.log(bombLine);
+const board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
