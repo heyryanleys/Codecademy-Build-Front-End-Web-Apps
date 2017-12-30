04e25f8935c4b0135300b5d4ecd4ee027939a743
heyryanleys <ryanleys@gmail.com>
2017-12-30 12:03:56 -0500

    Adding bombs randomly

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,12 +1,32 @@
 const generatePlayerBoards = (numberOfRows, numberOfColumns) => {
   let board = [];
-  for (i = 0; i < numberOfRows.length; i++){
+  for (i = 0; i < numberOfRows; i++){
     let row = [];
-    for (j=0; j < numberOfColumns.length; j++){
+    for (j = 0; j < numberOfColumns; j++){
       row.push(' ');
     }
-    board.push(row);
+      board.push(row);
   }
+  return board
+};
+
+const generatebombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for (i = 0; i < numberOfRows; i++){
+      let row = [];
+      for (j = 0; j < numberOfColumns; j++){
+        row.push(null);
+      }
+        board.push(row);
+    }
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombs < numberOfBombs){
+      let randomRowIndex = Math.floor(Math.random() * numberOfRows;)
+    }
+
+
+    return board
 }
 
-generatePlayerBoards(10,20)
+console.log(generatePlayerBoards('3','3'));
+console.log(generatebombBoard('3','3'));
