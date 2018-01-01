f82ab33d8617297af5ba7df3475e669af5ef1742
heyryanleys <ryanleys@gmail.com>
2018-01-01 15:49:33 -0500

    Bomb board generates but sometimes one bomb is overwriting an exisiting bomb

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -10,4 +10,24 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   return board
 }
 
-console.log(generatePlayerBoard(3,4))
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (i = 0; i < numberOfRows; i++){
+    let row = [];
+    for (j=0; j < numberOfColumns; j++){
+      row.push(null);
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs){
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced = numberOfBombsPlaced + 1;
+  }
+  return board
+}
+
+console.log(generatePlayerBoard(3,4));
+console.log(generateBombBoard(4,4,4));
