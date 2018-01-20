161e2b82afc6a2c32f85d5ac20103fa85df3bd69
heyryanleys <ryanleys@gmail.com>
2018-01-20 10:05:43 -0500

    Creating an array that generates results rather than giving static results

--- unit-2/day-8-9-ravenous-project/ravenous/src/App.js
+++ unit-2/day-8-9-ravenous-project/ravenous/src/App.js
@@ -5,8 +5,30 @@ import Business from './components/Business/Business.js'
 import BusinessList from './components/BusinessList/BusinessList.js'
 import SearchBar from './components/SearchBar/SearchBar.js'
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+];
+
 export default class App extends Component {
   render() {
+    <BusinessList businesses={businesses} />
     return (
       <div className="App">
         <h1>ravenous</h1>
