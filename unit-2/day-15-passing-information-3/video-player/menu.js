7d4497159dccf9dc7283a50a6f2e35774ac5c198
heyryanleys <ryanleys@gmail.com>
2018-01-21 16:09:50 -0500

    Almost done with video player app, not sure why it isn't working

---
+++ unit-2/day-15-passing-information-3/video-player/menu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export class Menu extends React.Component {
+  handleClick: function(e){
+    let text = e.target.value;
+    this.props.chooseVideo(text)
+  }
+
+	constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  render() {
+    return (
+      <form onClick={this.handleClick}>
+        <input type="radio" name="src" value="fast" /> fast
+        <input type="radio" name="src" value="slow" /> slow
+        <input type="radio" name="src" value="cute" /> cute
+        <input type="radio" name="src" value="eek" /> eek
+      </form>
+    );
+  }
+}
