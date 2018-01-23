f45926577d194259fee579336ddb2a135bd78c5c
heyryanleys <ryanleys@gmail.com>
2018-01-23 18:04:47 -0500

    CSS changes depending on click

--- unit-2/day-8-9-ravenous-project/ravenous/src/components/SearchBar/SearchBar.js
+++ unit-2/day-8-9-ravenous-project/ravenous/src/components/SearchBar/SearchBar.js
@@ -5,7 +5,7 @@ let sortByOptions = {
   'Best Match': 'best_match',
   'Highest Rated': 'rating',
   'Most Reviewed': 'review_count',
-}
+};
 
 class SearchBar extends React.Component {
   constructor(props){
@@ -15,24 +15,42 @@ class SearchBar extends React.Component {
       location: '',
       sortBy: 'best_match'
     }
-    this.handleSortByChange = this.handleSortByChange.bind(this)
-  }
-
-  renderSortByOptions(){
-    return Object.keys(sortByOptions).map(sortByOption => {
-        let sortByOptionValue = sortByOptions[sortByOption]
-        return <li key={sortByOptionValue}>{sortByOption}</li>
-    })
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.getSortByClass = this.getSortByClass.bind(this);
   }
 
   getSortByClass(sortByOption){
-    if this.state.sortBy == sortByOption ? return 'active' : else return ''
+    if (this.state.sortBy == sortByOption){
+      return 'active'
+    } else {
+      return ''
+    }
   }
 
   handleSortByChange(sortByOption){
     this.setState(
       { sortBy: sortByOption }
     )
+  }
+
+  handleTermChange(event){
+    this.setState(
+      { term: event.target.value }
+    )
+  }
+
+  handleLocationChange(event){
+    this.setState(
+      { location: event.target.value }
+    )
+  }
+
+  renderSortByOptions(){
+    return Object.keys(sortByOptions).map(sortByOption => {
+        let sortByOptionValue = sortByOptions[sortByOption]
+        return <li className={this.getSortByClass(sortByOptionValue)} onClick={this.handleSortByChange.bind(this, sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>
+    })
   }
 
   render(){
