1696a36651a6cc9243ffb1a83d60416adb80e8db
heyryanleys <ryanleys@gmail.com>
2018-01-29 17:56:30 -0500

    Accessing yelp objects

--- unit-2/day-8-9-ravenous-project/ravenous/src/util/Yelp.js
+++ unit-2/day-8-9-ravenous-project/ravenous/src/util/Yelp.js
@@ -7,5 +7,24 @@ let Yelp = {
 function search(term,location,sortBy){
   return fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}sort_by=${sortBy}',{
     headers: 'Bearer ${apiKey}'
+  }).then(response => {
+    return response.json();
+  }).then(jsonResponse => {
+    if(jsonResponse.businesses){
+      return jsonResponse.businesses.map(business => (
+        return {
+          id: jsonResponse.businesses.id,
+          imageSrc: jsonResponse.businesses.image_url,
+          name: jsonResponse.businesses.name,
+          address: jsonResponse.businesses.location.address1,
+          city: jsonResponse.businesses.location.city,
+          state: jsonResponse.businesses.location.state,
+          zipCode: jsonResponse.businesses.location.zip_code,
+          category: jsonResponse.businesses.categories.title,
+          rating: jsonResponse.businesses.rating,
+          reviewCount: jsonResponse.businesses.review_count
+        }
+      ))
+    } throw new Error('Request failed!')
   })
 }
