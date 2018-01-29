8322bb87138a893c8cb72f2427a9fc464f31e3dd
heyryanleys <ryanleys@gmail.com>
2018-01-29 17:59:33 -0500

    exporting yelp.js

--- unit-2/day-8-9-ravenous-project/ravenous/src/util/Yelp.js
+++ unit-2/day-8-9-ravenous-project/ravenous/src/util/Yelp.js
@@ -1,30 +1,28 @@
 const apiKey = 'U8IoCcWFZwxvmnhFR9v9iUQDBSalCn0jUxQoguRvM7zn0uBIISgGGVq2Mn0rVpz83aFE7bl8u0W6yN71mUzmQh5yC7qx-CQ8Hd-Y7XVvodnhE-YsbrboKHRTLKJvWnYx'
 
-let Yelp = {
-
-}
-
-function search(term,location,sortBy){
-  return fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}sort_by=${sortBy}',{
-    headers: 'Bearer ${apiKey}'
-  }).then(response => {
-    return response.json();
-  }).then(jsonResponse => {
-    if(jsonResponse.businesses){
-      return jsonResponse.businesses.map(business => (
-        return {
-          id: business.id,
-          imageSrc: business.image_url,
-          name: business.name,
-          address: business.location.address1,
-          city: business.location.city,
-          state: business.location.state,
-          zipCode: business.location.zip_code,
-          category: business.categories.title,
-          rating: business.rating,
-          reviewCount: business.review_count
-        }
-      ))
-    } throw new Error('Request failed!')
-  })
+export let Yelp = {
+  function search(term,location,sortBy){
+    return fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}sort_by=${sortBy}',{
+      headers: 'Bearer ${apiKey}'
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if(jsonResponse.businesses){
+        return jsonResponse.businesses.map(business => (
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories.title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }
+        ))
+      } throw new Error('Request failed!')
+    })
+  }
 }
