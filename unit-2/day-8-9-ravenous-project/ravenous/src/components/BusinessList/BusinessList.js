97db1252ff12d871a6c66f93a630d2488c310660
heyryanleys <ryanleys@gmail.com>
2018-01-29 18:32:09 -0500

    more troubleshooting

--- unit-2/day-8-9-ravenous-project/ravenous/src/components/BusinessList/BusinessList.js
+++ unit-2/day-8-9-ravenous-project/ravenous/src/components/BusinessList/BusinessList.js
@@ -7,7 +7,7 @@ class BusinessList extends React.Component {
     return(
       <div className="BusinessList">
         {this.props.businesses.map(business => {
-          return <Business business = {business} />})
+          return <Business business={business} key={business.id} />})
         }
       </div>
     )
