bcf59c30ab705ffd864a44fde93d0e54b89c5e3b
heyryanleys <ryanleys@gmail.com>
2018-01-29 18:20:29 -0500

    debugging

--- unit-2/day-8-9-ravenous-project/ravenous/src/App.js
+++ unit-2/day-8-9-ravenous-project/ravenous/src/App.js
@@ -4,17 +4,10 @@ import './App.css';
 import Business from './components/Business/Business.js'
 import BusinessList from './components/BusinessList/BusinessList.js'
 import SearchBar from './components/SearchBar/SearchBar.js'
+import Yelp from './util/Yelp.js'
 
 const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
+
 };
 
 const businesses = [
@@ -27,15 +20,24 @@ const businesses = [
 ];
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {businesses: []};
+    this.searchYelp = this.searchYelp.bind(this)
+  }
   searchYelp(term, location, sortBy){
-    console.log('Searching yelp with ' + term + ', ' + location + ', ' + sortBy)
+    Yelp.search(term,location,sortBy).then(businesses => {
+      this.setState({
+        businesses: businesses
+      })
+    })
   }
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={businesses} key={business.id}/>
       </div>
     );
   }
--- unit-2/day-8-9-ravenous-project/ravenous/src/util/Yelp.js
+++ unit-2/day-8-9-ravenous-project/ravenous/src/util/Yelp.js
@@ -1,14 +1,16 @@
 const apiKey = 'U8IoCcWFZwxvmnhFR9v9iUQDBSalCn0jUxQoguRvM7zn0uBIISgGGVq2Mn0rVpz83aFE7bl8u0W6yN71mUzmQh5yC7qx-CQ8Hd-Y7XVvodnhE-YsbrboKHRTLKJvWnYx'
 
-export let Yelp = {
-  function search(term,location,sortBy){
-    return fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}sort_by=${sortBy}',{
-      headers: 'Bearer ${apiKey}'
+export default let Yelp = {
+  search(term,location,sortBy){
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}sort_by=${sortBy}`,{
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
     }).then(response => {
       return response.json();
     }).then(jsonResponse => {
       if(jsonResponse.businesses){
-        return jsonResponse.businesses.map(business => (
+        return jsonResponse.businesses.map(business => {
           return {
             id: business.id,
             imageSrc: business.image_url,
@@ -21,7 +23,7 @@ export let Yelp = {
             rating: business.rating,
             reviewCount: business.review_count
           }
-        ))
+        })
       } throw new Error('Request failed!')
     })
   }
