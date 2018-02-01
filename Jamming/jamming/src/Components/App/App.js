96c2666f2daee60740c56ea3ab6be7748032f5a3
heyryanleys <ryanleys@gmail.com>
2018-02-01 17:24:53 -0500

    Starting to add JSX to app.js

---
+++ Jamming/jamming/src/Components/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <!-- Add a SearchBar component -->
+          <div className="App-playlist">
+          <!-- Add a SearchResults component -->
+          <!-- Add a Playlist component -->
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
