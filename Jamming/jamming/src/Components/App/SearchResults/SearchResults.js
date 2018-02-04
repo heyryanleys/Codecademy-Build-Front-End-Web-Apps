43212e8334c0ae6030e97821f1ba3420e9e243a5
heyryanleys <ryanleys@gmail.com>
2018-02-04 15:58:17 -0500

    Adding code for components

---
+++ Jamming/jamming/src/Components/App/SearchResults/SearchResults.js
@@ -0,0 +1,8 @@
+class SearchResults extends React.Component{
+  <div className="SearchResults">
+    <h2>Results</h2>
+    <!-- Add a TrackList component -->
+  </div>
+}
+
+export default SearchResults
