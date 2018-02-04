9c28fdc5227a7302553fb1665dd212193a476c85
heyryanleys <ryanleys@gmail.com>
2018-02-04 15:51:02 -0500

    Outlining components needed

---
+++ Jamming/jamming/src/Components/App/SearchBar/SearchBar.js
@@ -0,0 +1,8 @@
+class SearchBar extends React.Component {
+  render(){
+    <div class="SearchBar">
+      <input placeholder="Enter A Song, Album, or Artist" />
+      <a>SEARCH</a>
+    </div>
+  }
+}
